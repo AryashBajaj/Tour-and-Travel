38fe15cac09225a709e0f2720a2c9cb41a453126
AryashBajaj <aryashbajaj@gmail.com>
2023-11-30 03:33:40 +0530

    commit

--- index.php
+++ index.php
@@ -104,7 +104,7 @@ session_start();
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#food">Category</a></li>
-                    <li><a href="#food-menu">Menu</a></li>
+                    <li><a href="search.php">Search Menu</a></li>
                     <li><a href="#testimonials">Testimonial</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
